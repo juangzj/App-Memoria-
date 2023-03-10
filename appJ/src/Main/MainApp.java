83e1622f2224e60063ab0cdfd2376aa8c58abbf5
juanzgj <juanjosegoyeszambrano@gmail.com>
2023-03-09 23:34:33 -0500

    Aplicacion de juego para el entrenamiento de la memoria

---
+++ appJ/src/Main/MainApp.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+import VistaInicio.interfazInicio;
+
+/**
+ *
+ * @author JuanG
+ * Clase main para ejecutar el programa
+ */
+public class MainApp {
+    public static void main(String[]args){
+        
+    // llamar a la interfaz de inicio  
+    interfazInicio interfazIni = new interfazInicio();
+     interfazIni.setVisible(true);
+     interfazIni.setLocationRelativeTo(null);
+    
+    }
+}
